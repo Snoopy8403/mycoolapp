d6221681ef31309e4beb97eaef54213355c786bb
Snoopy8403 <gomba.laszlo84@gmail.com>
2025-12-30 13:10:26 +0100

    create

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -1,13 +1,30 @@
 package com.luv2code.springboot.demo.mycoolapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello(){
-        return "Hello";
+        return "Hellobello";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Go RUN!";
+    }
+
+    @GetMapping("/getCoach")
+    public String getCoach(){
+        return "Coach: " + this.coachName + " Team: " + this.teamName;
     }
 }
